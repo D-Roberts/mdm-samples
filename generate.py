7aad55ec4089671487f9d5e6ea45a69e6def0d10
Denisa Roberts <denisa.roberts@denisaroberts.me>
2025-11-18 19:30:20 -0500

    Entropy for last step only

--- generate.py
+++ generate.py
@@ -169,11 +169,11 @@ def generate_with_margin(
             x0 = torch.where(mask_index, x0, x)
             x0_p = margin_function(p[:, prompt.shape[1] :])
 
-            entropy = -entropy_function(p[:, prompt.shape[1] :])
-            # sum over the sequence
-            print(
-                f" for step {i} entropy for the sequence is ******************** {entropy.sum()/128}"
-            )
+            if i == steps - 1:
+                entropy = -entropy_function(p[:, prompt.shape[1] :]).sum() / gen_length
+                print(
+                    f" for step {i} entropy for the sequence is ********************{entropy}"
+                )
 
             confidence = torch.where(mask_index[:, prompt.shape[1] :], x0_p, -np.inf)
 
